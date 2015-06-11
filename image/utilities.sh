1020169914362a1a329e4026c88c07c680087bf0
Goran Cetusic <goran.cetusic@gmail.com>
2015-06-11 16:43:28 +0200

    using debian - removed all unnecessary stuff

--- image/utilities.sh
+++ image/utilities.sh
@@ -4,7 +4,5 @@ source /build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less nano vim psmisc tcpdump inetutils-tools inetutils-ping
-
-## This tool runs a command as another user and sets $HOME.
-cp /build/bin/setuser /sbin/setuser
+$minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
+iputils-arping iputils-tracepath
