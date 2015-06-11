172e70538e5189c7b4aa408df8c86fdbbfd2257f
Goran Cetusic <goran.cetusic@gmail.com>
2015-06-11 19:54:54 +0200

    added net-tools package that has netstat and ifconfig

--- image/utilities.sh
+++ image/utilities.sh
@@ -5,4 +5,4 @@ set -x
 
 ## Often used tools.
 $minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
-iputils-arping iputils-tracepath
+iputils-arping iputils-tracepath net-tools
