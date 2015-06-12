8a98409d2b70a318c6e1ec16b1b703c42594bd4b
Goran Cetusic <goran.cetusic@gmail.com>
2015-06-12 17:49:13 +0200

    added dhclient to package list

--- image/utilities.sh
+++ image/utilities.sh
@@ -5,4 +5,4 @@ set -x
 
 ## Often used tools.
 $minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
-iputils-arping iputils-tracepath net-tools file telnet
+iputils-arping iputils-tracepath net-tools file telnet isc-dhcp-client
