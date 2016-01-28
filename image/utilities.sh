706f3a487bd725159078566e94d727d85258d607
denisSal <denis.sale@gmail.com>
2016-01-28 17:25:39 +0100

    added tayga package for nat64

--- image/utilities.sh
+++ image/utilities.sh
@@ -7,4 +7,4 @@ set -x
 $minimal_apt_get_install curl less nano vim psmisc tcpdump iputils-ping \
 iputils-arping iputils-tracepath net-tools file telnet isc-dhcp-client nmap \
 dnsutils netcat man dsniff traceroute ettercap-text-only tcpreplay hping3 p0f \
-lsof strace elinks iperf iftop scapy bsd-mailx ndisc6 radvd iptables
+lsof strace elinks iperf iftop scapy bsd-mailx ndisc6 radvd iptables tayga
