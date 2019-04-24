f10957ea8a2f55b9792b948758612952306f3241
oikuda <valter.vasic@gmail.com>
2019-04-24 15:38:15 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 NAME="imunes/vroot"
-tag="debian-8"
+folder="debian-8"
+tag="latest"
 
 if [[ -n "$1" ]]; then
     tag="$1"
 fi
 
 if [[ -d "$tag" ]]; then
+    folder="$tag"
     NAME="$NAME:$tag"
 fi
 
-echo "[+] Entering dir '$tag' and building."
-cd $tag && sudo docker build -t $NAME .
+echo "[+] Entering dir '$folder' and building '$NAME'."
+cd $folder && sudo docker build -t $NAME .
 echo "[+] Built image '$NAME'."
