85a512555f703d692979a10704732165e4158eca
oikuda <valter.vasic@gmail.com>
2019-04-26 09:58:01 +0200

    add makefile, improve build script

---
+++ GNUmakefile
@@ -0,0 +1,36 @@
+NAME = imunes/template
+subdirs = $(filter-out common/, $(filter %/, $(wildcard */)))
+TAGS = $(subdirs:%/=%)
+clean_TAGS = $(addprefix clean_,$(TAGS))
+push_TAGS = $(addprefix push_,$(TAGS))
+
+.PHONY: $(TAGS) $(clean_TAGS) $(push_TAGS)
+
+info:
+	@echo	"To build an image use: make name"
+	@echo	"To remove an image use: make clean_name"
+	@echo	"To push an image use: make push_name"
+	@echo	"To build all images use: make build_all"
+	@echo	"To remove all images use: make clean_all"
+	@echo	"To push all images use: make push_all"
+
+$(TAGS):
+	./build.sh $@
+
+$(clean_TAGS):
+	docker rmi $(NAME):$(patsubst clean_%,%,$@)
+
+$(push_TAGS):
+	docker push $(NAME):$(patsubst push_%,%,$@)
+
+build_all: $(TAGS)
+
+clean_all:
+	for tag in $(TAGS); do \
+		docker rmi $(NAME):$$tag ; \
+	done 
+
+push_all:
+	for tag in $(TAGS); do \
+		docker push $(NAME):$$tag ; \
+	done 
--- build.sh
+++ build.sh
@@ -6,22 +6,28 @@ NAME="imunes/template"
 # - https://github.com/docker/for-linux/issues/73
 # - https://github.com/docker/for-linux/issues/502
 ULIMITS="--ulimit nofile=10240:10240 --ulimit nproc=65356:65536"
+DEFAULT_FOLDER="debian-8"
 
 tag="latest"
-folder="debian-8"
-
 if [[ -n "$1" ]]; then
     tag="$1"
 fi
 
 if [[ -d "$tag" ]]; then
     folder="$tag"
-    NAME="$NAME:$tag"
+else
+    if [[ "$tag" == "latest" ]]; then
+        folder=$DEFAULT_FOLDER
+    else
+        echo "[x] Tag '$tag' does not exist."
+        exit
+    fi
 fi
 
+image_name="$NAME:$tag"
 start=$(date +%s.%N)
-echo "[+] Entering dir '$folder' and building '$NAME'."
-sudo docker build $ULIMITS -t $NAME . -f $folder/Dockerfile
+echo "[+] Building '$image_name' from '$folder'."
+docker build $ULIMITS -t $image_name . -f $folder/Dockerfile
 end=$(date +%s.%N)
 runtime=$(echo "scale=5; (${end} - ${start})/1" | bc)
-echo "[+] Built image '$NAME' in $runtime s."
+echo "[+] Built image '$image_name' in $runtime s."
